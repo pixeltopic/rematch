35b4d8a82d589deacb8af9e6d11abd25ff6d82a3
pixeltopic <32804347+pixeltopic@users.noreply.github.com>
2020-06-13 14:50:41 -0700

    add shunting validation and table driven tests

--- expr_test.go
+++ expr_test.go
@@ -1,7 +1,6 @@
 package requery
 
 import (
-	"fmt"
 	"testing"
 )
 
@@ -116,25 +115,170 @@ import (
 //}
 
 func TestShuntRedux(t *testing.T) {
-	table := []struct {
-		input string
-		expr  string
-	}{
-		{
-			input: "((hi?the*re+(foo|bar)))()()", // not sure what reduceExpr will do near the end here.
-			expr:  "",
-		},
-		// also test '('
-	}
+	//table := []struct {
+	//	input string
+	//	expr  string
+	//}{
+	//	{
+	//		input: "((hi?the*re+(foo|bar)+mycake))", // these still need to be validated before getting passed into shunt2!
+	//		expr:  "",
+	//	},
+	//	// also test '('
+	//}
 
-	for i, entry := range table {
-		t.Run("test shunting", func(t *testing.T) {
-			q, err := shunt2(tokenizeExpr(entry.input))
-			if err != nil {
-				fmt.Printf("shunt2 test #%d had an error: %v\n", i+1, err)
-			} else {
-				fmt.Printf("tok test #%d: actual expr=%s expected expr=%s\n", i+1, q.Join(","), entry.expr)
-			}
-		})
-	}
+	t.Run("expression conversion to reverse polish notation tests", func(t *testing.T) {
+		entries := []struct {
+			in         string
+			out        string // comma joined queue output
+			shouldFail bool
+			errStr     string
+		}{
+			{
+				in:  "hi?the***re",
+				out: "hi?the*re/r",
+			},
+			{
+				in:  "((hi?the***re))",
+				out: "hi?the*re/r",
+			},
+			{
+				in:         "((hi?the***re))+",
+				shouldFail: true,
+				errStr:     "unexpected operator at end of expression, want operand",
+			},
+			{
+				in:  "((hi?the***re+*kekw?))",
+				out: "hi?the*re/r,*kekw?/r,+",
+			},
+			{
+				in:         "k+|+kekw+",
+				shouldFail: true,
+				errStr:     "unexpected infix operator, want operand",
+			},
+			{
+				in:  "kkekw|(foobar)",
+				out: "kkekw,foobar,|",
+			},
+			{
+				in:         "|",
+				shouldFail: true,
+				errStr:     "unexpected infix operator, want operand",
+			},
+			{
+				in:         "***",
+				shouldFail: true,
+				errStr:     "invalid word; cannot be lone asterisk wildcard",
+			},
+			{
+				in:         "?",
+				shouldFail: true,
+				errStr:     "invalid word; cannot be lone question wildcard",
+			},
+			{
+				in:  "((hi?the***re+*a?))",
+				out: "hi?the*re/r,*a?/r,+",
+			},
+			{
+				in:         "kekw)(())",
+				shouldFail: true,
+				errStr:     "mismatched parenthesis",
+			},
+			{
+				in:         "(())",
+				shouldFail: true,
+				errStr:     "unexpected right parenthesis",
+			},
+			{
+				in:         "(",
+				shouldFail: true,
+				errStr:     "unexpected operator at end of expression, want operand",
+			},
+			{
+				in:         "(hi",
+				shouldFail: true,
+				errStr:     "mismatched parenthesis at end of expression",
+			},
+			{
+				in:         "(hi|",
+				shouldFail: true,
+				errStr:     "unexpected operator at end of expression, want operand",
+			},
+			{
+				in:         "|hi)",
+				shouldFail: true,
+				errStr:     "unexpected infix operator, want operand",
+			},
+			{
+				in:         "hi+hi1)",
+				shouldFail: true,
+				errStr:     "mismatched parenthesis",
+			},
+			{
+				in:         "(hi++hi1)",
+				shouldFail: true,
+				errStr:     "unexpected infix operator, want operand",
+			},
+			{
+				in:  "(hi0+hi1|hi2+hi3)",
+				out: "hi0,hi1,+,hi2,|,hi3,+",
+			},
+			{
+				in:  "((dog+(hotate|TETAHO))|(g*D+(Xpotato|yubiyubi)))",
+				out: "dog,hotate,TETAHO,|,+,g*D/r,Xpotato,yubiyubi,|,+,|",
+			},
+			{
+				in:         "((dog+(hotate|TETAHO))| (g*D+(Xpotato|yubiyubi)))",
+				shouldFail: true,
+				errStr:     "invalid char in word; must be alphanumeric",
+			},
+			{
+				in:         "((dog+(hotate|TETAHO&))|(g*D+(Xpotato|yubiyubi)))",
+				shouldFail: true,
+				errStr:     "invalid char in word; must be alphanumeric",
+			},
+			{
+				in:         ")(())",
+				shouldFail: true,
+				errStr:     "unexpected right parenthesis",
+			},
+			{
+				in:  "(hi)|((guys+hows+it+goin))",
+				out: "hi,guys,hows,+,it,+,goin,+,|",
+			},
+			{
+				in:         "(**)",
+				shouldFail: true,
+				errStr:     "invalid word; cannot be lone asterisk wildcard",
+			},
+		}
+
+		for i, entry := range entries {
+			t.Run("test shunting", func(t *testing.T) {
+				q, err := ExprToRPN(entry.in)
+				switch err {
+				case nil:
+					if entry.shouldFail {
+						t.Errorf("test #%d should have err='%s', but err=nil", i+1, entry.errStr)
+					}
+				default:
+					if entry.shouldFail {
+						if entry.errStr != err.Error() {
+							t.Errorf("test #%d should have err='%s', but err='%s'", i+1, entry.errStr, err.Error())
+						}
+					} else {
+						t.Errorf("test #%d should have err=nil, but err='%s'", i+1, err.Error())
+					}
+				}
+
+				switch q {
+				case nil:
+				default:
+					if actualOut := q.Join(","); actualOut != entry.out {
+						t.Errorf("test #%d should have out=%s, but out=%s", i+1, entry.out, actualOut)
+					}
+				}
+			})
+		}
+	})
+
 }
--- shunt.go
+++ shunt.go
@@ -2,15 +2,37 @@ package requery
 
 import (
 	"errors"
+	"fmt"
 	"strings"
 
 	"github.com/pixeltopic/requery/utils"
 )
 
-func tokenizeExpr(expr string) *utils.Queue {
+func tokenizeExpr(expr string) (*utils.Queue, error) {
 	tokenQueue := utils.NewQueue()
 
-	var token strings.Builder
+	var (
+		token  strings.Builder
+		adjAst bool
+	)
+
+	flushWordTok := func() error {
+		if token.Len() != 0 {
+			switch tokStr := token.String(); tokStr {
+			case string(OPWILDCARDAST):
+				return errors.New("invalid word; cannot be lone asterisk wildcard")
+			case string(OPWILDCARDQUEST):
+				return errors.New("invalid word; cannot be lone question wildcard")
+			default:
+				if strings.Contains(tokStr, string(OPWILDCARDAST)) || strings.Contains(tokStr, string(OPWILDCARDQUEST)) {
+					tokStr = tokStr + "/r"
+				}
+				tokenQueue.Enqueue(tokStr)
+				token.Reset()
+			}
+		}
+		return nil
+	}
 
 	for i := 0; i < len(expr); i++ {
 		switch char := rune(expr[i]); char {
@@ -21,23 +43,47 @@ func tokenizeExpr(expr string) *utils.Queue {
 		case OPAND:
 			fallthrough
 		case OPOR:
-			if token.Len() != 0 {
-				tokenQueue.Enqueue(token.String())
-				token.Reset()
+			if err := flushWordTok(); err != nil {
+				return nil, err
 			}
 			tokenQueue.Enqueue(string(char))
-		default:
+			adjAst = false
+		case OPWILDCARDAST:
+			if !adjAst {
+				token.WriteRune(char)
+				adjAst = true
+			}
+		case OPWILDCARDQUEST:
 			token.WriteRune(char)
+			adjAst = false
+		default:
+			if !allowedWordChars(char) {
+				return nil, errors.New("invalid char in word; must be alphanumeric")
+			}
+			token.WriteRune(char)
+			adjAst = false
 		}
 	}
+	if err := flushWordTok(); err != nil {
+		return nil, err
+	}
 
-	return tokenQueue
+	return tokenQueue, nil
 }
 
 // shunt2 produces a queue ordered in reverse polish notation; will err if unbalanced parenthesis
 func shunt2(tokens *utils.Queue) (*utils.Queue, error) {
 	rpnQueue := utils.NewQueue()
 	opStack := utils.NewStack()
+
+	const (
+		expectOperator = 0
+		expectOperand  = 1
+	)
+
+	var state int // false = expectOperator, true = expectOperand state
+
+	state = expectOperand
 
 	stackPeek := func() string {
 		e, _ := opStack.Peek()
@@ -47,6 +93,8 @@ func shunt2(tokens *utils.Queue) (*utils.Queue, error) {
 	for tokens.Len() > 0 {
 		switch tok, _ := tokens.Dequeue(); tok {
 		case string(OPAND):
+			// AND and OR infix operators have EQUAL precendence, meaning the expression will be evaluated from left to right during absence of groups.
+			// ambiguity can be reduced by using parens
 			fallthrough
 		case string(OPOR):
 			/*
@@ -54,15 +102,26 @@ func shunt2(tokens *utils.Queue) (*utils.Queue, error) {
 					pop operators from the operator stack onto the output queue.
 				push it onto the operator stack.
 			*/
+			if state != expectOperator {
+				return nil, errors.New("unexpected infix operator, want operand")
+			}
 			for opStack.Len() > 0 && stackPeek() != string(OPGROUPL) {
 				op, _ := opStack.Pop()
 				_ = rpnQueue.Enqueue(op)
 			}
 			_ = opStack.Push(tok)
-
+			state = expectOperand
 		case string(OPGROUPL):
+			if state != expectOperand {
+				return nil, errors.New("unexpected left parenthesis")
+			}
 			_ = opStack.Push(string(OPGROUPL))
+			state = expectOperand
 		case string(OPGROUPR):
+			if state != expectOperator {
+				return nil, errors.New("unexpected right parenthesis")
+			}
+
 			var lParenWasFound bool
 
 			// while the operator at the top of the operator stack is not a left parenthesis:
@@ -78,7 +137,7 @@ func shunt2(tokens *utils.Queue) (*utils.Queue, error) {
 			}
 			// If the stack runs out without finding a left parenthesis, then there are mismatched parentheses.
 			if !lParenWasFound {
-				return nil, errors.New("mismatched parens")
+				return nil, errors.New("mismatched parenthesis")
 			}
 
 			// if there is a left parenthesis at the top of the operator stack, then:
@@ -89,17 +148,29 @@ func shunt2(tokens *utils.Queue) (*utils.Queue, error) {
 					_, _ = opStack.Pop()
 				}
 			}
+
+			state = expectOperator
 		default:
+			if state != expectOperand {
+				return nil, errors.New("unexpected operand, want operator")
+			}
 			// the token is not an operator; but a word.
 			// this needs validation (make sure wildcards and allowed chars are valid)
+			// append /r to end of tok if regex
 			rpnQueue.Enqueue(tok)
+			state = expectOperator
 		}
 	}
+
+	if state != expectOperator {
+		return nil, errors.New("unexpected operator at end of expression, want operand")
+	}
+
 	/* After while loop, if operator stack not null, pop everything to output queue */
 	for opStack.Len() > 0 {
 		ele, _ := opStack.Pop()
 		if ele == string(OPGROUPL) || ele == string(OPGROUPR) {
-			return nil, errors.New("mismatched parens at end")
+			return nil, errors.New("mismatched parenthesis at end of expression")
 		}
 		_ = rpnQueue.Enqueue(ele)
 	}
@@ -107,22 +178,42 @@ func shunt2(tokens *utils.Queue) (*utils.Queue, error) {
 	return rpnQueue, nil
 }
 
+// ExprToRPN converts an expression into reverse polish notation.
+func ExprToRPN(expr string) (*utils.Queue, error) {
+	q, err := tokenizeExpr(expr)
+	if err != nil {
+		return nil, err
+	}
+
+	return shunt2(q)
+}
+
 func parseRPN(rpnQueue *utils.Queue, textTokens []string) bool {
 	argStack := utils.NewStack()
+	var curResult bool
 
 	for rpnQueue.Len() > 0 {
 		switch tok, _ := rpnQueue.Peek(); tok {
-		case string(OPGROUPL):
-			fallthrough
-		case string(OPGROUPR):
-			fallthrough
 		case string(OPAND):
 			fallthrough
 		case string(OPOR):
+			if argStack.Len() < 2 {
+				return false // parse error
+			}
+			lhs, _ := argStack.Pop()
+			rhs, _ := argStack.Pop()
+
+			// AND:
+			// check if lhs and rhs are both present in text tokens (if not $Y or $N); if yes, generate a special "$Y" token and push into stack
+			// if lhs and rhs contain $N, $Y in one or both pops, determine which one "wins". in this case, if it is not ($Y, $Y) or ($Y, present), or (present, $Y),
+			// it will eval to $N before pushing back onto the stack and looking for the next arg
+			fmt.Println(lhs, rhs) // temp
+
 		default:
-			argStack.Push(tok)
+			_, _ = rpnQueue.Dequeue()
+			_ = argStack.Push(tok)
 		}
-		rpnQueue.Dequeue()
+
 	}
-	return false
+	return curResult
 }
